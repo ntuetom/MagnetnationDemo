4ffef3fcb74ca40e0707d646190b8c2a2bd90dc9
ntuetom <tomwu1013@gmail.com>
2014-05-21 14:17:07 +0800

    3animalfinal

--- Assets/Script/Magmusic.cs
+++ Assets/Script/Magmusic.cs
@@ -4,109 +4,88 @@ using System.Collections;
 public class Magmusic : MonoBehaviour {
 	public static Vector3 deltaCompass;   //初始磁感應 
 	public Vector3 ChageCompass;   //當下磁感應
-	public Vector3 OneCompass; 
-	bool startgo=false;
+	public Vector3 OneCompass;
+    bool startgo = false;
+    bool[] bplay = new bool[3]{true,true,true};
     public AudioClip[] aud;
-    float felez,ftiger;
+    float felez, ftiger;
     Vector3[] V3temp;
 	// Use this for initialization
 	void Start () { 
 		// 取得預設 磁感應大小
         deltaCompass = Vector3.zero;
+  
 	}
 	void OnEnable(){
 		//取得電子羅盤裝置, 才能偵測磁感應
 		Input.location.Start();
 		Input.compass.enabled = true;
-        OneCompass = new Vector3(8,-2,-20);
+        OneCompass = new Vector3(0,0,0);
         startgo = true;
 	}
 	// Update is called once per frame
 	void Update () {
 
 		if(startgo){
-
-
 			ChageCompass = Input.compass.rawVector;
             deltaCompass = ChageCompass - OneCompass;
 						
             //elephant
-            if ((ChageCompass.x - OneCompass.x) < 0 && (ChageCompass.x - OneCompass.x) > -20 &&
-                (ChageCompass.y - OneCompass.y) > -50 && (ChageCompass.y - OneCompass.y) < -10)
+            if (deltaCompass.z>10 && deltaCompass.x>0 && deltaCompass.y<-10)
             {
-                        if (!audio.isPlaying)
+                bplay[1] = true;
+                bplay[2] = true;
+                if (!audio.isPlaying && bplay[0])
                         {
                             audio.clip = aud[0];
                             audio.Play();
+                            bplay[0] = false;
                         }
-						transform.position=new Vector3(-20,80,0);
+						transform.position=new Vector3(-15,60,0);
                         felez = ChageCompass.z;
 				
 			}
            
             //turtle
-            else if ((ChageCompass.x - OneCompass.x) > -10 && (ChageCompass.x - OneCompass.x) < 0 &&
-                      (ChageCompass.y - OneCompass.y) > -20 && (ChageCompass.y - OneCompass.y) < -10 &&
-                        ChageCompass.z < felez)
+            else if (deltaCompass.z>-5 && deltaCompass.z<10 && deltaCompass.y>=-10)
             {
-                if (!audio.isPlaying)
+                bplay[0] = true;
+                bplay[2] = true;
+                if (!audio.isPlaying && bplay[1])
                 {
-                    audio.clip = aud[2];
+                    audio.clip = aud[1];
                     audio.Play();
+                    bplay[1] = false;
                 }
-                transform.position = new Vector3(0, 50, 0);
+                transform.position = new Vector3(0, 35, 0);
 
 
             }
            
-            //giraffe
-            else if ((ChageCompass.x - OneCompass.x) < -40 && (ChageCompass.x - OneCompass.x) > -70 &&
-                (ChageCompass.y - OneCompass.y) > 10 && (ChageCompass.y - OneCompass.y) < 20)
-            {
-                if (!audio.isPlaying)
-                {
-                    audio.clip = aud[1];
-                    audio.Play();
-                }
-                transform.position = new Vector3(-10, 20, 0);
-
-
-            }
+           
             
              //tiger
-            else if ((ChageCompass.x - OneCompass.x) > 0 && (ChageCompass.y - OneCompass.y) < 10 &&
-                       (ChageCompass.y - OneCompass.y) < 0 && (ChageCompass.y - OneCompass.y) > -10)
+            else if (deltaCompass.z<=-5)
             {
-
-                if (!audio.isPlaying)
+                bplay[1] = true;
+                bplay[0] = true;
+                if (!audio.isPlaying && bplay[2])
                 {
-                    audio.clip = aud[3];
+                    audio.clip = aud[2];
                     audio.Play();
+                    bplay[2] = false;
                 }
-                transform.position = new Vector3(20, 70, 0);
+                transform.position = new Vector3(20, 55, 0);
                 ftiger = ChageCompass.z;
             }
             
-            //flamingo
-            else if ((ChageCompass.x - OneCompass.x) > 0 && (ChageCompass.x - OneCompass.x) < 10 &&
-                     (ChageCompass.y - OneCompass.y) < -8 && (ChageCompass.y - OneCompass.y) > -15 &&
-                      ChageCompass.z > ftiger)
-            {
-                if (!audio.isPlaying)
-                {
-                    audio.clip = aud[4];
-                    audio.Play();
-                }
-                transform.position = new Vector3(15, 30, 0);
-
-
-            }
-
             
             //歸零
-            else
+            if(deltaCompass.x<2 && deltaCompass.x>-2 && deltaCompass.y<2 && deltaCompass.y>-2 && deltaCompass.z<2 && deltaCompass.z>-2)
             {
-
+                bplay[1] = true;
+                bplay[0] = true;
+                bplay[2] = true;
                 audio.Stop();
 				transform.position=new Vector3(-30,8,0);
 				
